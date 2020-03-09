3d20191e126f72e7e79af9729b8d49f65d1f6e50
Laura Donado <ldonado11@gmail.com>
2020-03-09 09:47:04 -0500

    Create component for path-not-found and added components to module

--- health-entity-web/src/app/app.module.ts
+++ health-entity-web/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from './shared/login/login.component';
+import { PathNotFoundComponent } from './shared/path-not-found/path-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
