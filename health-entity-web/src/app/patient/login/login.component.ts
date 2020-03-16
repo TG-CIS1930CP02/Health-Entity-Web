70991ede13c9d2401735bd9ba6a5aae5323143ba
Laura Donado <ldonado11@gmail.com>
2020-03-15 21:06:48 -0500

    Add method for loging in login component

--- health-entity-web/src/app/patient/login/login.component.ts
+++ health-entity-web/src/app/patient/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { PatientService } from '../../services/patient.service';
+import { Patient } from '../../models/patient';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router, private patientService: PatientService) { }
 
-  ngOnInit(): void {
-  }
+  patient: Patient = null;
+  email: string = null;
+  password: string = null;
 
+  incorrectLogin = false;
+
+  ngOnInit(): void { }
+
+  login() {
+    this.loginService.login(this.email, this.password)
+      .subscribe(data => {
+        this.incorrectLogin = false;
+        this.patientService.findByEmailPassword(this.email, this.password)
+          .subscribe(result => {
+            this.patient = result;
+
+          },
+          error => {
+            console.error(error);
+        });
+      },
+      error => {
+        this.incorrectLogin = true;
+        console.error(error);
+      });
+    }
 }
