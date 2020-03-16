b18116e134a7411d2c31df2173b48cb1dc290893
Laura Donado <ldonado11@gmail.com>
2020-03-15 21:06:22 -0500

    Changed routing to use new login

--- health-entity-web/src/app/app-routing.module.ts
+++ health-entity-web/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PathNotFoundComponent } from './shared/path-not-found/path-not-found.component';
-import { LoginComponent } from './shared/login/login.component';
+import { LoginComponent } from './patient/login/login.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/login' },
-  { path: 'login', component:  LoginComponent},
+  { path: '', pathMatch: 'full', redirectTo: '/patientLogin' },
+  { path: 'patientLogin', component:  LoginComponent},
   { path: '**', component: PathNotFoundComponent }
 ];
 
