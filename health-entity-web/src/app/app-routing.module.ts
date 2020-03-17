403bcd97059cf802ed68812217d95c534e95409a
Laura Donado <ldonado11@gmail.com>
2020-03-17 18:30:29 -0500

    Add login in routing and add different text to differentiate logins

--- health-entity-web/src/app/app-routing.module.ts
+++ health-entity-web/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PathNotFoundComponent } from './shared/path-not-found/path-not-found.component';
 import { LoginPatientComponent } from './patient/login-patient/login-patient.component';
+import { LoginPractitionerComponent } from './practitioner/login-practitioner/login-practitioner.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/inicio' },
   { path: 'login-patient', component:  LoginPatientComponent},
+  { path: 'login-practitioner', component: LoginPractitionerComponent},
   { path: '**', component: PathNotFoundComponent }
 ];
 
