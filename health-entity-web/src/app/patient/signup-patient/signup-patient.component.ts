a2d1167696b700299d8a03628b931918ca097dcc
Laura Donado <ldonado11@gmail.com>
2020-03-31 19:20:38 -0500

    Implement signup component for patient

--- health-entity-web/src/app/patient/signup-patient/signup-patient.component.ts
+++ health-entity-web/src/app/patient/signup-patient/signup-patient.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from '../../models/patient';
+import { OptionsList } from '../../models/options-lists';
+import { PatientService } from '../../services/patient.service';
 
 @Component({
   selector: 'app-signup-patient',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-patient.component.css']
 })
 export class SignupPatientComponent implements OnInit {
+  patientFound: Patient = new Patient(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
+  );
+  found = 'pending';
+  idType = 'Selecciona un tipo';
+  id: number;
 
-  constructor() { }
+  options = OptionsList.identificationTypes;
 
-  ngOnInit(): void {
+  constructor(private patientService: PatientService) { }
+
+  ngOnInit(): void { }
+
+  search() {
+    this.patientService.findByIdentification(this.idType, this.id).subscribe(
+      result => {
+        console.log(result);
+        this.patientFound = result;
+        this.found = 'found';
+      },
+      error => {
+        console.error(error);
+        this.found = 'not_found';
+      }
+    );
   }
 
+  close() {
+    this.found = 'pending';
+  }
 }
