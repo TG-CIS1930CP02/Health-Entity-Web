0ab892fb9e193bea77f75dd1e7ff80a3b64d0f1e
Laura Donado <ldonado11@gmail.com>
2020-03-31 19:21:11 -0500

    Implement details component for patient

--- health-entity-web/src/app/patient/details-patient/details-patient.component.ts
+++ health-entity-web/src/app/patient/details-patient/details-patient.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Patient } from '../../models/patient';
+import { ContactPoint } from '../../models/contactPoint';
+import { OptionsList } from '../../models/options-lists';
+import { PatientService } from '../../services/patient.service';
+import { Address } from 'app/models/address';
 
 @Component({
   selector: 'app-details-patient',
@@ -6,10 +12,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-patient.component.css']
 })
 export class DetailsPatientComponent implements OnInit {
+  @Input()
+  patient: Patient = new Patient(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
+  );
 
-  constructor() { }
+  telecoms: ContactPoint[] = [];
+  addresses: Address[] = [];
+  cities = OptionsList.cities;
+
+  incorrectSignup = false;
+  successSignup = false;
+
+  systems = OptionsList.contactPointSystems;
+  uses = OptionsList.contactPointUses;
+  addressUses = OptionsList.addressUses;
+
+  constructor(
+    private patientService: PatientService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.telecoms = this.patient.telecoms;
+    this.addresses = this.patient.addresses;
   }
 
+  signup() {
+    // TODO validate value in telecoms
+    this.patient.active = true;
+    this.patient.telecoms = this.telecoms;
+    this.patient.addresses = this.addresses;
+
+    this.patientService.createPatient(this.patient).subscribe(
+      result => {
+        console.log(result);
+        this.successSignup = true;
+      },
+      error => {
+        console.error(error);
+        this.incorrectSignup = true;
+      }
+    );
+  }
+
+  closeError() {
+    this.incorrectSignup = false;
+  }
+
+  closeSuccess() {
+    this.successSignup = false;
+    this.router.navigate(['/patient/home']);
+  }
+
+  addContactPoint() {
+    this.telecoms.push(new ContactPoint('Selecciona un sistema', 'Selecciona un tipo', null));
+  }
+
+  deleteContactPoint() {
+    this.telecoms.splice(- 1, 1);
+  }
+
+  addAddress() {
+    this.addresses.push(new Address('Selecciona una ciudad', 'Selecciona un tipo', '', '', 'Colombia'));
+  }
+
+  deleteAddress() {
+    this.addresses.splice(- 1, 1);
+  }
+
+  trackByIndex(index: number, obj: any): any {
+    return index;
+  }
 }
