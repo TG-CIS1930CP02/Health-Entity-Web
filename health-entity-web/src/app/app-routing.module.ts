e11b07ad94fd07d529604dbfff1b4a9ecfa27f70
Laura Donado <ldonado11@gmail.com>
2020-04-12 16:00:24 -0500

    Implement patient home

--- health-entity-web/src/app/app-routing.module.ts
+++ health-entity-web/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { SignupAdministrativeAssistantComponent } from './components/administrat
 import { HomeAdministratorComponent } from './components/administrator/home-administrator/home-administrator.component';
 import { HomePractitionerComponent } from './components/practitioner/home-practitioner/home-practitioner.component';
 import { HomeAdministrativeAssistantComponent } from './components/administrative-assistant/home-administrative-assistant/home-administrative-assistant.component';
+import { HomePatientComponent } from './components/patient/home-patient/home-patient.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
@@ -22,6 +23,7 @@ const routes: Routes = [
   { path: 'admin/home', component: HomeAdministratorComponent},
   { path: 'practitioner/home', component: HomePractitionerComponent},
   { path: 'admin-assistant/home', component: HomeAdministrativeAssistantComponent},
+  { path: 'patient/home', component: HomePatientComponent},
   { path: '**', component: PathNotFoundComponent }
 ];
 
