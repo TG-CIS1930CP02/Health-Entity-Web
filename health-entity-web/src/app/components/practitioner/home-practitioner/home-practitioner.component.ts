fa1b75719f1e363c43fb148f97c93b543f117755
Laura Donado <ldonado11@gmail.com>
2020-04-13 22:14:13 -0500

    Change practitioner home to follow template

--- health-entity-web/src/app/components/practitioner/home-practitioner/home-practitioner.component.ts
+++ health-entity-web/src/app/components/practitioner/home-practitioner/home-practitioner.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { OptionsList } from '../../../models/options-lists';
-import { PatientService } from '../../../services/patient.service';
-import { Patient } from '../../../models/patient';
-import { ContactPoint } from '../../../models/contactPoint';
-import { Address } from 'app/models/address';
 
 @Component({
   selector: 'app-home-practitioner',
@@ -11,37 +6,7 @@ import { Address } from 'app/models/address';
   styleUrls: ['./home-practitioner.component.css']
 })
 export class HomePractitionerComponent implements OnInit {
-  patientFound: Patient = new Patient(
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined
-  );
-  found = 'pending';
-  idType = 'Selecciona un tipo';
-  id: number;
-
-  options = OptionsList.identificationTypes;
-
-  constructor(private patientService: PatientService) { }
+  constructor() { }
 
   ngOnInit(): void { }
-
-  search() {
-    this.patientService.findByIdentification(this.idType, this.id).subscribe(
-      result => {
-        console.log(result);
-        this.patientFound = result;
-        this.found = 'found';
-      },
-      error => {
-        console.error(error);
-        this.found = 'not_found';
-      }
-    );
-  }
 }
