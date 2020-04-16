a9c386fe8d250bd1f44768f617a7bac4d24df561
Laura Donado <ldonado11@gmail.com>
2020-04-16 16:24:28 -0500

    Create component for creating observation

---
+++ health-entity-web/src/app/components/observation/create-observation/create-observation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-observation',
+  templateUrl: './create-observation.component.html',
+  styleUrls: ['./create-observation.component.css']
+})
+export class CreateObservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
