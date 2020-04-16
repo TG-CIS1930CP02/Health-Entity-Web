8079eda0d2545c0bbf760d6dbff8adb3c3683735
Laura Donado <ldonado11@gmail.com>
2020-04-16 17:33:02 -0500

    Create read observation component

---
+++ health-entity-web/src/app/components/observation/read-observation/read-observation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-read-observation',
+  templateUrl: './read-observation.component.html',
+  styleUrls: ['./read-observation.component.css']
+})
+export class ReadObservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
