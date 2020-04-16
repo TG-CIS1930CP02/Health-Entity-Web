8ae2472cf7e6172b556ed797e94406cc2105fbe9
Laura Donado <ldonado11@gmail.com>
2020-04-16 16:24:42 -0500

    Update model to use arrays

--- health-entity-web/src/app/models/diagnostic-report.ts
+++ health-entity-web/src/app/models/diagnostic-report.ts
@@ -5,16 +5,16 @@ export class DiagnosticReport {
     public identifier: number,
     public basedOn: string,
     public status: string,
-    public category: string,
+    public category: string[],
     public code: string,
     public subject: Identification,
     public effectiveDateTime: Date,
     public issued: Date,
-    public performer: Identification,
-    public resultsInterpreter: Identification,
-    public result: Observation,
-    public media: any,
+    public performer: Identification[],
+    public resultsInterpreter: Identification[],
+    public result: Observation[],
+    public media: string[],
     public conclusion: string,
-    public conclusionCode: string
+    public conclusionCode: string[]
   ) {}
 }
