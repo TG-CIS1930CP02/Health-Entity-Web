f6ca8a464d5bae5a287f6082060ad5668b9a4904
Laura Donado <ldonado11@gmail.com>
2020-04-18 16:05:39 -0500

    Changes to create component

--- health-entity-web/src/app/components/condition/create-condition/create-condition.component.ts
+++ health-entity-web/src/app/components/condition/create-condition/create-condition.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Condition } from '../../../models/condition';
+import { OptionsList } from '../../../models/options-lists';
 
 @Component({
   selector: 'app-create-condition',
@@ -9,7 +11,39 @@ export class CreateConditionComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  condition: Condition = new Condition(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  );
 
+  clinicalStatusOptions = OptionsList.ConditionClinicalStatus;
+  verificationStatusOptions = OptionsList.ConditionVerificationStatus;
+  severityOptions = OptionsList.AllergyIntoleranceSeverity;
+  codeOptions = OptionsList.ClinicalFindingsCodes;
+  bodyOptions = OptionsList.BodyStructure;
+
+  firstTime: boolean;
+
+  ngOnInit(): void { }
+
+  record() {
+    // TODO validate all fields
+    if (this.firstTime) {
+      this.condition.recordedDate = new Date();
+    }
+    /* TODO sacar info de estos
+      recorder y subject
+    */
+
+    // TODO create service to record
+  }
 }
