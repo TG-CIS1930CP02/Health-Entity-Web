5949faf63bc4bb15681b90487f974ca0a6e99ff8
Laura Donado <ldonado11@gmail.com>
2020-04-20 08:56:58 -0500

    Implement read component for procedure

--- health-entity-web/src/app/components/procedure/read-procedure/read-procedure.component.ts
+++ health-entity-web/src/app/components/procedure/read-procedure/read-procedure.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Procedure } from 'app/models/procedure';
 
 @Component({
   selector: 'app-read-procedure',
@@ -8,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class ReadProcedureComponent implements OnInit {
 
   constructor() { }
+  @Input()
+  procedure: Procedure = new Procedure(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
+  );
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
