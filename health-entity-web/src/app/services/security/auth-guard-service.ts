239ed20239451d770b706dea68b1bcaa8ed93d80
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-04-25 16:22:57 -0500

    If the token doesn't exist, the user has no authorization

--- health-entity-web/src/app/services/security/auth-guard-service.ts
+++ health-entity-web/src/app/services/security/auth-guard-service.ts
@@ -11,6 +11,8 @@ export class RoleGuardService implements CanActivate {
     canActivate(route: ActivatedRouteSnapshot): boolean {
       const token = localStorage.getItem('token');
       const expectedAuthorities = route.data.expectedAuthorities;
+      if (token == null)
+        return false;
       if (!this.auth.isAuthenticated() && !this.hasRole(token, expectedAuthorities)) {
         this.router.navigate(['not-authorized']);
         return false;
