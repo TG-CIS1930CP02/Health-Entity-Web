f643609b9be8d46260e10ff349e6fdf8cc43dbf8
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-04-25 23:25:50 -0500

    Redirect to not authorized when there is no token

--- health-entity-web/src/app/services/security/auth-guard-service.ts
+++ health-entity-web/src/app/services/security/auth-guard-service.ts
@@ -11,8 +11,10 @@ export class RoleGuardService implements CanActivate {
     canActivate(route: ActivatedRouteSnapshot): boolean {
       const token = localStorage.getItem('token');
       const expectedAuthorities = route.data.expectedAuthorities;
-      if (token == null)
+      if (token == null){
+        this.router.navigate(['not-authorized']);
         return false;
+      }
       if (!this.auth.isAuthenticated() && !this.hasRole(token, expectedAuthorities)) {
         this.router.navigate(['not-authorized']);
         return false;
