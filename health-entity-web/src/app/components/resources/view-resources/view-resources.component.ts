c8d6d460ea953d8de9427b0863a70003ffc0e3c1
Pedro Santiago Lizarazo <p-lizarazo@javeriana.edu.co>
2020-05-02 18:03:47 -0500

    Added hash comparison logic on front

--- health-entity-web/src/app/components/resources/view-resources/view-resources.component.ts
+++ health-entity-web/src/app/components/resources/view-resources/view-resources.component.ts
@@ -93,8 +93,49 @@ export class ViewResourcesComponent implements OnInit {
         },
         error => {
           console.log(error);
+        },
+        async () => {
+          let jsonString = this.stringifyKeysInOrder(this.resource)
+          const hexHash = await this.digestMessage(jsonString);
+          console.log(hexHash);
         }
       );
     }
+    this.resource
+  }
+
+  recursivelyOrderKeys(unordered) {
+    // If it's an array - recursively order any
+    // dictionary items within the array
+    if (Array.isArray(unordered)) {
+      unordered.forEach( (item, index) => {
+        unordered[index] = this.recursivelyOrderKeys(item);
+      });
+      return unordered;
+    }
+  
+    // If it's an object - let's order the keys
+    if (typeof unordered === 'object') {
+      var ordered = {};
+      Object.keys(unordered).sort().forEach( (key) => {
+        ordered[key] = this.recursivelyOrderKeys(unordered[key]);
+      });
+      return ordered;
+    }
+  
+    return unordered;
+  };
+  
+  stringifyKeysInOrder(data) {
+    var sortedData = this.recursivelyOrderKeys(data);
+    return JSON.stringify(sortedData);
+  };
+  
+  async digestMessage(message) {
+    const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+    return hashHex;
   }
 }
